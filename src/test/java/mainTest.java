dbcdd6b8cd22deaa7a01506436d680bd4c543884
flysafely <616604060@qq.com>
2020-04-13 19:15:38 +0800

    第2次提交！!

--- src/test/java/mainTest.java
+++ src/test/java/mainTest.java
@@ -4,5 +4,8 @@ public class mainTest {
     @Test
     public void test1(){
         System.out.println("test!!");
+        System.out.println("test!!2");
+        System.out.println("test!!3");
+
     }
 }
