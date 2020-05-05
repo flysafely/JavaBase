962dc50b51254a9987ca0accc8268bba6ace7447
flysafely <616604060@qq.com>
2020-05-05 22:13:03 +0800

    2020.5.5-Java读写锁(ReentrantReadWriteLock)写锁和读锁之间的关系

--- src/main/java/JavaUtilConcurrent/ReentrantReadWriteLock/MainTest.java
+++ src/main/java/JavaUtilConcurrent/ReentrantReadWriteLock/MainTest.java
@@ -3,6 +3,7 @@ package JavaUtilConcurrent.ReentrantReadWriteLock;
 public class MainTest {
     public static void main(String[] args) {
         SourcesClass sourcesClass = new SourcesClass();
+
         new Thread(()->{
             for (int i = 0; i < 31;i++){
                 sourcesClass.put("第" + i + "个值", i*100);
--- src/main/java/JavaUtilConcurrent/ReentrantReadWriteLock/SourcesClass.java
+++ src/main/java/JavaUtilConcurrent/ReentrantReadWriteLock/SourcesClass.java
@@ -20,14 +20,23 @@ public class SourcesClass {
             /**
              * 模拟操作时延，能够正常打印完整的写入情况
              */
-            TimeUnit.MILLISECONDS.sleep(100);
+//            TimeUnit.MILLISECONDS.sleep(100);
             System.out.println(Thread.currentThread().getName() + "线程已经写入完成！");
         }catch (Exception e){
             System.out.println(e);
         }finally {
+            /**
+             * 读锁是需要等待写锁释放后才能获得
+             * 释放掉写锁的一瞬间也可能有线程能
+             * 获取到读锁
+             * 所以结果不一定是 全部写入操作循环 或者线程执行完毕后才有 读取的操作
+             * 但是能够保证的是写锁中的代码只能有一个线程进行操作
+             *
+             * AAA线程正在写入--->第xx个值：xxx
+             * AAA线程已经写入完成！
+             */
             rwlock.writeLock().unlock();
         }
-
     }
 
     public void get(String key){
@@ -35,7 +44,7 @@ public class SourcesClass {
         try {
             System.out.println(Thread.currentThread().getName() + "线程正在读取--->" + key);
             Object result = map.get(key);
-            TimeUnit.MILLISECONDS.sleep(100);
+//            TimeUnit.MILLISECONDS.sleep(100);
             System.out.println(Thread.currentThread().getName() + "线程已经读取--->" + key + ":" + result.toString());
         }catch (Exception e){
             System.out.println(e);
