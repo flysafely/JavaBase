8c194c4c92ae3a40716c838bcbe7807250edc71c
flysafely <616604060@qq.com>
2020-05-07 01:04:54 +0800

    2020.5.7-双冒号语法糖在forEach函数中的特殊含义

--- src/main/java/JavaUtilConcurrent/CountDownLatch/JoinDemo.java
+++ src/main/java/JavaUtilConcurrent/CountDownLatch/JoinDemo.java
@@ -25,10 +25,36 @@ public class JoinDemo implements Runnable{
             thlist.add(new Thread(new JoinDemo(), String.valueOf(i)));
         }
 
+
         /**
          * start方法是实例方法没有异常抛出，可以直接使用双冒号调用
+         *
+         * 两种写法等价
+         * foreach中接收一个Consumer<? super E> action的入参
+         *     Consumer函数式接口中的抽象方法accept(E e)的参数就是lambda表达式中参数th的类型所限制的
+         *     又因为th的类型就是 E，所以Consumer<? super E>的含义就是 一个Consumer函数式接口的accept方法所需要的参数是Thread类或者其父类
+         *     双冒号的写法前后含义就是：
+         *        Thread代表foreach中的每个对象的类型
+         *        starat代表foreach中每个对象的实例方法
+         *        不是一个静态方法的引用
          */
-        thlist.forEach(Thread::start);
+        thlist.forEach((th)->{th.start();});
+//        thlist.forEach(Thread::start);
+        /**
+         * 两种写法等价
+         * 注意和上面的区别
+         *     一个foreach遍历的对象作为System.out.println()方法中的参数
+         *     上面foreach遍历的对象作为需要被调用start实例方法的对象
+         *     双冒号的写法前后含义就是：
+         *        System.out代表这个类
+         *        println代表了System.out的一个静态方法
+         *        是一个正常的静态方法的引用
+         *
+         */
+        thlist.forEach((th)->{ System.out.println(th);});
+//        thlist.forEach(System.out::println);
+
+
         /**
          * 需要抛出异常的方法，不能使用双冒号的方式
          */
